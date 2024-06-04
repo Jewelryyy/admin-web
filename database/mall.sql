97e163e8d8e12e28ff0eac16aa438c17486388b7
Jewelryyy <3118758526@qq.com>
2024-06-04 16:33:18 +0800

    数据库文件更新

--- database/mall.sql
+++ database/mall.sql
@@ -11,11 +11,66 @@
  Target Server Version : 80032 (8.0.32)
  File Encoding         : 65001
 
- Date: 24/05/2024 15:58:35
+ Date: 04/06/2024 16:31:56
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for category
+-- ----------------------------
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category`  (
+  `categoryId` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `level` int NOT NULL DEFAULT 1,
+  `goodsNum` int NOT NULL DEFAULT 0,
+  `unit` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `isEnabled` tinyint NOT NULL DEFAULT 1,
+  `sortOrder` int NOT NULL DEFAULT 1,
+  `parentId` int NOT NULL DEFAULT 0,
+  `desc` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`categoryId`) USING BTREE,
+  INDEX `parentCategory`(`parentId` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 38 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of category
+-- ----------------------------
+INSERT INTO `category` VALUES (5, '服装', 1, 100, '件', 1, 1, 0, '服装');
+INSERT INTO `category` VALUES (6, '手机数码', 1, 100, '件', 1, 1, 0, '手机数码');
+INSERT INTO `category` VALUES (7, '家用电器', 1, 100, '件', 1, 1, 0, '家用电器');
+INSERT INTO `category` VALUES (8, '家具家装', 1, 100, '件', 1, 1, 0, '家具家装');
+INSERT INTO `category` VALUES (9, '汽车用品', 1, 100, '件', 1, 1, 0, '汽车用品');
+INSERT INTO `category` VALUES (10, '电脑办公', 1, 0, '件', 1, 1, 0, '电脑办公');
+INSERT INTO `category` VALUES (11, '外套', 2, 100, '件', 1, 1, 5, '外套');
+INSERT INTO `category` VALUES (12, 'T恤', 2, 100, '件', 1, 1, 5, 'T恤');
+INSERT INTO `category` VALUES (13, '休闲裤', 2, 100, '件', 1, 1, 5, '休闲裤');
+INSERT INTO `category` VALUES (14, '牛仔裤', 2, 100, '件', 1, 1, 5, '牛仔裤');
+INSERT INTO `category` VALUES (15, '衬衫', 2, 100, '件', 1, 1, 5, '衬衫');
+INSERT INTO `category` VALUES (16, '手机通讯', 2, 100, '件', 1, 1, 6, '手机通讯');
+INSERT INTO `category` VALUES (17, '手机配件', 2, 100, '件', 1, 1, 6, '手机配件');
+INSERT INTO `category` VALUES (18, '摄影摄像', 2, 100, '件', 1, 1, 6, '摄影摄像');
+INSERT INTO `category` VALUES (19, '影音娱乐', 2, 100, '件', 1, 1, 6, '影音娱乐');
+INSERT INTO `category` VALUES (20, '数码配件', 2, 100, '件', 1, 1, 6, '数码配件');
+INSERT INTO `category` VALUES (21, '电视', 2, 100, '件', 1, 1, 7, '电视');
+INSERT INTO `category` VALUES (22, '空调', 2, 100, '件', 1, 1, 7, '空调');
+INSERT INTO `category` VALUES (23, '洗衣机', 2, 100, '件', 1, 1, 7, '洗衣机');
+INSERT INTO `category` VALUES (24, '冰箱', 2, 100, '件', 1, 1, 7, '冰箱');
+INSERT INTO `category` VALUES (25, '厨卫大电', 2, 100, '件', 1, 1, 7, '厨卫大电');
+INSERT INTO `category` VALUES (26, '厨房卫浴', 2, 100, '件', 1, 1, 8, '厨房卫浴');
+INSERT INTO `category` VALUES (27, '灯饰照明', 2, 100, '件', 1, 1, 8, '灯饰照明');
+INSERT INTO `category` VALUES (28, '五金工具', 2, 100, '件', 1, 1, 8, '五金工具');
+INSERT INTO `category` VALUES (29, '卧室家具', 2, 100, '件', 1, 1, 8, '卧室家具');
+INSERT INTO `category` VALUES (30, '客厅家具', 2, 100, '件', 1, 1, 8, '客厅家具');
+INSERT INTO `category` VALUES (31, '全新整车', 2, 100, '件', 1, 1, 9, '全新整车');
+INSERT INTO `category` VALUES (32, '车载电器', 2, 100, '件', 1, 1, 9, '车载电器');
+INSERT INTO `category` VALUES (33, '维修保养', 2, 100, '件', 1, 1, 9, '维修保养');
+INSERT INTO `category` VALUES (34, '汽车装饰', 2, 100, '件', 1, 1, 9, '汽车装饰');
+INSERT INTO `category` VALUES (35, '平板电脑', 2, 100, '件', 1, 1, 10, '平板电脑');
+INSERT INTO `category` VALUES (36, '笔记本', 2, 100, '件', 1, 1, 10, '笔记本');
+INSERT INTO `category` VALUES (37, '硬盘', 2, 100, '件', 1, 1, 10, '硬盘');
 
 -- ----------------------------
 -- Table structure for menu
@@ -87,7 +142,7 @@ CREATE TABLE `rolemenu`  (
   INDEX `m`(`mid` ASC) USING BTREE,
   CONSTRAINT `m` FOREIGN KEY (`mid`) REFERENCES `menu` (`mid`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `r` FOREIGN KEY (`roleId`) REFERENCES `role` (`roleId`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 230 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 279 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of rolemenu
@@ -108,22 +163,22 @@ INSERT INTO `rolemenu` VALUES (44, 3, 11);
 INSERT INTO `rolemenu` VALUES (45, 3, 5);
 INSERT INTO `rolemenu` VALUES (46, 3, 12);
 INSERT INTO `rolemenu` VALUES (47, 3, 13);
-INSERT INTO `rolemenu` VALUES (214, 1, 2);
-INSERT INTO `rolemenu` VALUES (215, 1, 3);
-INSERT INTO `rolemenu` VALUES (216, 1, 4);
-INSERT INTO `rolemenu` VALUES (217, 1, 5);
-INSERT INTO `rolemenu` VALUES (218, 1, 7);
-INSERT INTO `rolemenu` VALUES (219, 1, 8);
-INSERT INTO `rolemenu` VALUES (220, 1, 9);
-INSERT INTO `rolemenu` VALUES (221, 1, 10);
-INSERT INTO `rolemenu` VALUES (222, 1, 11);
-INSERT INTO `rolemenu` VALUES (223, 1, 12);
-INSERT INTO `rolemenu` VALUES (224, 1, 13);
-INSERT INTO `rolemenu` VALUES (225, 1, 14);
-INSERT INTO `rolemenu` VALUES (226, 1, 15);
-INSERT INTO `rolemenu` VALUES (227, 1, 16);
-INSERT INTO `rolemenu` VALUES (228, 1, 6);
-INSERT INTO `rolemenu` VALUES (229, 1, 17);
+INSERT INTO `rolemenu` VALUES (263, 1, 2);
+INSERT INTO `rolemenu` VALUES (264, 1, 5);
+INSERT INTO `rolemenu` VALUES (265, 1, 6);
+INSERT INTO `rolemenu` VALUES (266, 1, 7);
+INSERT INTO `rolemenu` VALUES (267, 1, 12);
+INSERT INTO `rolemenu` VALUES (268, 1, 13);
+INSERT INTO `rolemenu` VALUES (269, 1, 14);
+INSERT INTO `rolemenu` VALUES (270, 1, 15);
+INSERT INTO `rolemenu` VALUES (271, 1, 16);
+INSERT INTO `rolemenu` VALUES (272, 1, 17);
+INSERT INTO `rolemenu` VALUES (273, 1, 3);
+INSERT INTO `rolemenu` VALUES (274, 1, 8);
+INSERT INTO `rolemenu` VALUES (275, 1, 9);
+INSERT INTO `rolemenu` VALUES (276, 1, 4);
+INSERT INTO `rolemenu` VALUES (277, 1, 10);
+INSERT INTO `rolemenu` VALUES (278, 1, 11);
 
 -- ----------------------------
 -- Table structure for user
@@ -143,7 +198,7 @@ CREATE TABLE `user`  (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'admin', 'jewelry@foxmail.com', '123456', '2024-05-17 20:04:34', '2024-05-22 21:48:21', 1);
+INSERT INTO `user` VALUES (1, 'admin', 'jewelry@foxmail.com', '123456', '2024-05-17 20:04:34', '2024-06-04 12:17:36', 1);
 INSERT INTO `user` VALUES (3, 'lujiewen', '21301131@bjtu.edu.cn', '123456', '2024-05-18 14:00:15', '2024-05-21 16:44:55', 1);
 INSERT INTO `user` VALUES (4, 'test', 'j2166383@gmail.com', 'wwwwww', '2024-05-18 14:01:43', NULL, 0);
 
